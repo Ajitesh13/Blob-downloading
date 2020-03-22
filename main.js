f6668e30b1b6d02864b36367b2b595df7973a775
Ajitesh13 <aji.yash13@gmail.com>
2020-03-22 22:41:02 +0530

    ES6 added to main.js

--- main.js
+++ main.js
@@ -5,20 +5,16 @@
 // 5. click event
 // 6. remove anchor tag and blob
 
-function download() {
+let download = document.getElementById('download');
+download.addEventListener('click', () => {
     const blob = new Blob(['This is a new File'], {type: "text/plain"});
     downloadFile(blob, "newFile.txt");
-}
+});
 
 function downloadFile(blob, filename) {
     const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-
-    // before click we need to add some prop to "a" tag
-    a.href = url;
-    a.download = filename;
-
-    //step5: click event
-    a.click();
-
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = filename;
+    anchor.click();
 }
