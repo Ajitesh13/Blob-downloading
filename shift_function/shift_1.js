b05a3df4227ac5985952aea62ba4bd8e39a27363
Ajitesh13 <aji.yash13@gmail.com>
2020-03-29 00:15:18 +0530

    shift_func added

---
+++ shift_function/shift_1.js
@@ -0,0 +1,11 @@
+let ar = [1,2,3,4,5];
+console.log(ar);
+let ar1 = ar.shift();
+console.log(ar);
+console.log(ar1);
+
+let json = [{"num1": 1},{"num2": 2},{"num3": 3}];
+console.log(json);
+let json1 = json.shift();
+console.log(json);
+console.log(json1);
